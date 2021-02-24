d010b6ce4ee54ae8496065e28acd037d297e0e3d
31702160323 <13005687228@163.com>
2021-02-24 15:56:01 +0800

    该项目是二开项目来的，因为忘了添加原项目地址，现在重新添加在main.js里面

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+说明：该项目是二开项目，我是把原来的项目的页面由vue页面改成nvue页面，再从代码进行了我自己认为的优化
+原作者: Darshan1358
+原项目源码: https://github.com/darshan1358/wan-android
+*/
+
 import Vue from 'vue'
 import App from './App'
 import store from './store'
